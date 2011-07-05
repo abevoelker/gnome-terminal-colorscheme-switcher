a042d7a014fe4ed1e3b009e4f5e4bea91a7509f1
Abe Voelker <abe@abevoelker.com>
2011-07-05 10:03:12 -0500

    Remove old version of ColorScheme

--- color_scheme.rb
+++ color_scheme.rb
@@ -1,5 +1,20 @@
-class ColorScheme
-  def palette
-    raise "Must implement a palette"
+module GnomeTermColors
+  class ColorScheme
+    def palette
+      raise "Must supply a palette"
+    end
+
+    def background_color
+      raise "Must supply a background color"
+    end
+
+    def foreground_color
+      raise "Must supply a foreground color"
+    end
+
+    def bold_color
+      raise "Must supply a bold color"
+    end
   end
 end
+
